ce1197abcad0ab6b8892c5321d8f420b31a69d30
Anthonyaba <393481321@qq.com>
2018-03-14 00:01:20 +0800

    create react app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,44 @@
-import React from 'react';
+
+import React, { Component } from 'react';
+
+
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+ 
+class Greeting extends Component 
+{
+  state=
+  {
+    who:'world'
+  }
+
+    switchName=()=>
+    {
+      if (this.state.who==='world')
+        this.setState((prevState,props)=>({who:'react'}))
+      else
+        this.setState((prevState,props)=>({who:'world'}))
+    }
+    render() 
+    {
+      return(
+        <div>  
+            <h1>{`hello ${this.state.who}`}</h1> 
+            <button onClick={this.switchName}> {this.state.who==='world'?'react':'world'}</button>
+        </div>
+      );
+    }
+
+}
+
+
+const App=(props,context)=>{
+
+  return (
+    <div>
+      <Greeting who={'world'} />
+      <Greeting who={'react'} />
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
